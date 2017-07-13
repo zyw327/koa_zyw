dd26dbbd6ef967ad109755c5443a22f18b827d9e
zyw327 <327523057@qq.com>
2017-07-13 11:00:51 +0800

    Update config.js

--- app/config/config.js
+++ app/config/config.js
@@ -1,29 +1,57 @@
-var mysql = require('mysql');
-module.exports = function (type) {
-
-	switch(type) {
-		case "mysql":
-			return mysql.createPool({
-				  connectionLimit : 2,
-				  host            : 'host',
-				  user            : 'username',
-				  password        : 'passwd',
-				  database        : 'database',
-				  debug           : false,
-				  multipleStatements: true
-			});
-			break;
-		case "mail":
-			return {
-			    host: 'smtp.email.com',
-			    port: 25,
-			    secure: false, // upgrade later with STARTTLS
-			    auth: {
-			        user: 'email_addr',
-			        pass: 'email_passwd'
-			    }
-			};
-			break;
-	}
-	return false;
-}
+module.exports = (function() {
+    let env = process.env.NODE_ENV;
+    let config = {
+        mysql: {
+            connectionLimit: 2,
+            host: 'host',
+            user: 'username',
+            password: 'password',
+            database: 'db',
+            debug: false,
+            multipleStatements: true
+        },
+        mail: {
+            host: 'host',
+            port: 25,
+            secure: false, // upgrade later with STARTTLS
+            auth: {
+                user: 'email_addr',
+                pass: 'email_passwd'
+            }
+        },
+        server: {
+            port: '3000'
+        },
+        mongo: {
+            user: '',
+            password: '',
+            database: ''
+        },
+        redis: {
+            host: 'host',
+            port: '6379',
+            detect_buffers: false,
+            connect_timeout: 3600000,
+            retry_strategy: 5,
+            password: 'password',
+            db: '2'
+        }
+    };
+    if (env == 'production') {
+        return config;
+    } else if (env == 'development') {
+        config.server.port = '3006';
+        config.mysql = {
+            connectionLimit: 2,
+                host: 'host',
+                user: 'username',
+                password: 'password',
+                database: 'db',
+                debug: false,
+                multipleStatements: true
+        };
+        return config;
+    } else {
+        return config;
+    }
+})();
